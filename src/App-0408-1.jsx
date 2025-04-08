88aac0b869d905c63da657d2f4a7a99fe1cad79d
TiffanyYang411 <suhanyang902@gmail.com>
2025-04-08 17:10:44 +0800

    react-1

---
+++ src/App-0408-1.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <hi>Hi, React</hi>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum maxime repellendus excepturi, repellat eaque, esse placeat soluta minima fuga omnis porro cupiditate, neque fugit veritatis delectus quo tempore. Tempore, nesciunt!</p>
+        </>
+    )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import App from './App-0408-2.jsx'
+// import './index.css'
+import './style-0408.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
