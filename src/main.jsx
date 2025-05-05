47d51fea2a790cac786dd68a9548e48332ced6ca
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-05 12:12:20 +0800

    0505-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import App from './0429/App-faq.jsx'
+import App from './0505/event-1.html'
 // import './index.css'
 import './style-0408.css'
 import { BrowserRouter } from 'react-router-dom'
